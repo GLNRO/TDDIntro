90ca4face034bdced36e1660fe317c6cbdc969ad
GLNRO <lg@glnro.com>
2016-02-22 12:32:03 -0500

    Write factorial methods, all tests pass

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -1,7 +1,16 @@
 package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        return -1;
+    public Integer compute(int i)
+    {   int factorialNum = 1;
+        if(i < 0){
+            throw new IllegalArgumentException("Try a positive value...");
+        }
+        else if(i > 0){
+            for(int j=1;j<=i;j++) {
+                factorialNum *= j;
+            }
+        }
+        return factorialNum;
     }
 }
